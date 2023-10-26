fa7635c19dc8c2e85056abc6b5368659ee527b72
Lukáš Kratochvíl <kratochvil.luke@gmail.com>
2023-10-26 13:48:17 +0200

    ...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,19 +17,37 @@
     let newChildNodeName: string;
     let parentNodeName: string;
 
-    const addNode = (parentNodeName: string,
-        currentNode: TreeNodeInfo,
-        nodeToAdd: TreeNodeInfo) : TreeNodeInfo => {
-        return currentNode.name === parentNodeName
-          ? { ...currentNode, childNodes: [...currentNode.childNodes, nodeToAdd] }
-          : {
-              ...currentNode,
-              childNodes: currentNode.childNodes.map((node) => addNode(parentNodeName, node, nodeToAdd))
-            }
-      };
+    const addNodeBtnA_OnClick = () => {
+        const findNode = (name: string, nodes: TreeNodeInfo[]): TreeNodeInfo | undefined => {          
+            for (const node of nodes) {
+            if (node.name === name) return node;            
+            const foundNode = findNode(name, node.childNodes);
+            if (foundNode) return foundNode;
+            }  
+            return undefined;
+        }
+
+        state.update(curr => {         
+            findNode(parentNodeName, curr)?.childNodes.push({ isRoot: false, name: newChildNodeName, childNodes: []});
+            return curr;
+        })
+
+        console.log($state); 
+    }
 
     // Calling this function also messes up the store, because after the call the second method (addNodeBtnB_OnClick) also stops working
-    const addNodeBtnA_OnClick = () => {
+    const addNodeBtnB_OnClick = () => {
+        const addNode = (parentNodeName: string,
+            currentNode: TreeNodeInfo,
+            nodeToAdd: TreeNodeInfo) : TreeNodeInfo => {
+            return currentNode.name === parentNodeName
+            ? { ...currentNode, childNodes: [...currentNode.childNodes, nodeToAdd] }
+            : {
+                ...currentNode,
+                childNodes: currentNode.childNodes.map((node) => addNode(parentNodeName, node, nodeToAdd))
+                }
+        };
+
         state.update(
           curr => curr.map((node => addNode(parentNodeName, node, { isRoot: false, name: newChildNodeName, childNodes: []}))
         ));
@@ -37,23 +55,7 @@
         console.log($state); // The store is updated, but reactivity is not triggered at all
     }
 
-    const addNodeBtnB_OnClick = () => {
-        const findNode = (name: string, nodes: TreeNodeInfo[]): TreeNodeInfo | undefined => {          
-          for (const node of nodes) {
-            if (node.name === name) return node;            
-            const foundNode = findNode(name, node.childNodes);
-            if (foundNode) return foundNode;
-          }  
-          return undefined;
-        }
 
-        state.update(curr => {         
-          findNode(parentNodeName, curr)?.childNodes.push({ isRoot: false, name: newChildNodeName, childNodes: []});
-          return curr;
-        })
-
-        console.log($state); 
-    }
   </script>
   
   <main>
@@ -65,8 +67,8 @@
       <span>PARENT NODE NAME:</span>
       <input bind:value={parentNodeName}>
     </div>
-    <button on:click={addNodeBtnA_OnClick}>Add Node (not working)</button> <!-- This is not working at all -->
-    <button on:click={addNodeBtnB_OnClick}>Add Node (kinda working)</button> <!-- The node is added, but it's visible only after reopenning the node -->
+    <button on:click={addNodeBtnB_OnClick}>Add Node (not working)</button> <!-- This is not working at all -->
+    <button on:click={addNodeBtnA_OnClick}>Add Node (kinda working)</button> <!-- The node is added, but it's visible only after reopenning the node -->
     <Tree state={state} />
   </main>
 
--- src/routes/Tree.svelte
+++ src/routes/Tree.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { getContext } from "svelte";
     import  TreeNode from "./TreeNode.svelte";
     import type { TreeNodeInfo } from "./TreeNodeInfo";
     import type { Writable} from "svelte/store";
